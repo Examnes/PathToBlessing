2a38ff6fb3b396b82663e5643c898a77402a851d
Examnes <CLIKateiDanbo@gmail.com>
2019-07-29 21:58:42 +0800

    huh

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local ptb = RegisterMod("PathToBlessing");
 local test_item = Isaac.GetItemIdByName("Desu");
 function ptb:use_test( )	
-    Isaac.DebugString("I am exist!");
+    Isaac.DebugString("I am exit!");
 end
 
 ptb:AddCallback( ModCallbacks.MC_USE_ITEM, ptb.use_test, test_item ); 
