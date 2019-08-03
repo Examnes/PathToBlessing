93aa088079413ebf7070d2e82733cb15256b6a3e
Examnes <CLIKateiDanbo@gmail.com>
2019-08-03 18:50:51 +0800

    changed something

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+
+--TODO:перенести реализацию в timers.lua (я хз как)
 local TManager = {timers = {}};
 TManager.__index = TManager;
 
@@ -19,25 +21,31 @@ function TManager:addTimer(timer_name,duration,extra_data,timer_callback)
     TManager.timers[timer_name] = timer;
 end
 
+
+
 local ptb = RegisterMod("PathToBlessing");
 
 
 ptb.COLL_TEST = Isaac.GetItemIdByName("Desu");
 ptb.TEST_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/copypaste.anm2");
 ptb.invulnerability = false;
- 
+ptb.hasTest = false;
+ptb.TestBaff = false;
 
 function ptb:use_test( )	
-    ptb.player:AddNullCostume(ptb.TEST_COSTUME)
+    if not ptb.hasTest then ptb.hasTest = true end
+    Isaac.GetPlayer(0):AddNullCostume(ptb.TEST_COSTUME);
     ptb.invulnerability = true;
-    ptb.player:AddCacheFlags(CacheFlag.CACHE_DAMAGE)
-    ptb.player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
-	ptb.player:EvaluateItems()
-    TManager:addTimer("test_item_use",120,nil,function()
-        
+    ptb.TestBaff = true;
+    Isaac.GetPlayer(0):AddCacheFlags(CacheFlag.CACHE_DAMAGE);
+    Isaac.GetPlayer(0):AddCacheFlags(CacheFlag.CACHE_FIREDELAY);
+    Isaac.GetPlayer(0):AddCacheFlags(CacheFlag.CACHE_FLYING);
+	Isaac.GetPlayer(0):EvaluateItems();
+    TManager:addTimer("test_item_use",120,nil,function();
         ptb.invulnerability = false;
-        ptb.player:TryRemoveNullCostume(ptb.TEST_COSTUME);
-        ptb.player:EvaluateItems()
+        ptb.TestBaff = false;
+        Isaac.GetPlayer(0):TryRemoveNullCostume(ptb.TEST_COSTUME);
+        Isaac.GetPlayer(0):EvaluateItems();
     end)
 end
 
@@ -46,25 +54,25 @@ function ptb:mainLoop()
 end
 
 function ptb:onCache(EntityPlayer, Cache)
-    if ptb.invulnerability then  
+    Isaac.DebugString(tostring(ptb.TestBaff).." "..tostring(EntityPlayer))
+    if ptb.TestBaff then  
         if Cache == CacheFlag.CACHE_DAMAGE then 
-            EntityPlayer.Damage = EntityPlayer.Damage + 5 
+            EntityPlayer.Damage = EntityPlayer.Damage+5; 
         end
         if Cache == CacheFlag.CACHE_FIREDELAY then 
-            EntityPlayer.FireDelay = EntityPlayer.FireDelay - 5 
+            EntityPlayer.MaxFireDelay = EntityPlayer.MaxFireDelay-5;
         end
-        EntityPlayer.CanFly = true;
+        if Cache == CacheFlag.CACHE_FLYING then
+            EntityPlayer.CanFly = true;
+        end
     end
 end
 
-function ptb:onDamage(TookDamage, DamageAmount, DamageFlag, DamageSource, DamageCountdownFrames)
+function ptb:onDamag(TookDamage, DamageAmount, DamageFlag, DamageSource, DamageCountdownFrames)
     if ptb.invulnerability then return true end
 end
 
-function ptb:onStart(_,bool)
-    ptb.player = Isaac.GetPlayer(0);
-    ptb:AddCallback( ModCallbacks.MC_ENTITY_TAKE_DMG,ptb.onDamage,ptb.player)
-end
+ptb:AddCallback( ModCallbacks.MC_ENTITY_TAKE_DMG,ptb.onDamag,Isaac.GetPlayer(0))
 
 ptb:AddCallback( ModCallbacks.MC_USE_ITEM, ptb.use_test, ptb.COLL_TEST ); 
 
